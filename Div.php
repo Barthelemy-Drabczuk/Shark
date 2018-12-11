a4845b4bb7251da7a4e332dc4488cceec892b4c7
DRABCZUK Barthelemy <d17010509@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-11 13:14:09 +0100

    ajout de la classe Div

---
+++ Div.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d17010509
+ * Date: 11/12/18
+ * Time: 13:03
+ */
+
+class Div {
+
+    /**
+     * Div constructor.
+     */
+    public function __construct() {
+    }
+}
